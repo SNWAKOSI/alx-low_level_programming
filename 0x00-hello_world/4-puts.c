c07a47bb79fdecff86d8cf740987bd2ab1254db2
snwakosi <snwakosi@gmail.com>
2023-03-16 15:07:01 -0700

    Hello Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - for the Hello puts
+ *
+ * Return:i value 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
