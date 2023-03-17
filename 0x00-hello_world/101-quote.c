f05deb5f12003b915d47f77e39443e36d9993c0b
snwakosi <snwakosi@gmail.com>
2023-03-16 17:09:06 -0700

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - a program that gives a standard error
+ *
+ * Return: output must return 1
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
