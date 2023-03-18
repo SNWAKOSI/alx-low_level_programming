412c472fc15e33bc869c31df7c41c85323ed8cc7
snwakosi <snwakosi@gmail.com>
2023-03-17 17:09:22 -0700

    printing letters in lower cases

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabets in lowercase
+ *
+ * Return: must alwsys be 0
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
