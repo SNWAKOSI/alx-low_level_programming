3149a76bdc04f89d58fa3bc34f2ec105dba0340b
snwakosi <snwakosi@gmail.com>
2023-03-21 08:37:15 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - to print alphabets
+ * Return: must be 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
