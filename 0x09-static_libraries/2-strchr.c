0ae8aee698a326dc87fc3d5d22a40ea72187d73a
snwakosi <snwakosi@gmail.com>
2023-04-10 15:55:30 -0700

    0*09

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - point of entry
+ * @s: input value
+ * @c: input value
+ * Return: when successful, print 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - detects lowercase character
+ * @c: parameters to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
