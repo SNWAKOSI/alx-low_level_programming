1b02a888175d8253dddef35bcd93d3a1e25426e1
snwakosi <snwakosi@gmail.com>
2023-04-13 15:10:28 -0700

    malloc

---
+++ 0x0C-more_malloc_free/3-main.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - buffer appears in hexa
+ * @buffer: the address of memory we printing
+ * @size: the size of the memory we printing
+ *
+ * Return: Nothing.
+ */
+
+void simple_print_buffer(int *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: retuen 0 for success
+ */
+
+int main(void)
+{
+    int *a;
+
+    a = array_range(0, 10);
+    simple_print_buffer(a, 11);
+    free(a);
+    return (0);
+}
