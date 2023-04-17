874cf8fbf4fb47b5ef38f155497a096167becd8c
snwakosi <snwakosi@gmail.com>
2023-04-17 02:02:41 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file
+ *
+ * Return: must be 0 for success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: Returns 0 for success
+ */
+
+int main(void)
+{
+    int s;
+
+    s = SUM(98, 1024);
+    printf("%d\n", s);
+    return (0);
+}
