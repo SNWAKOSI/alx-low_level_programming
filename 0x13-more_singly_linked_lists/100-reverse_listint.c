b89d7e51b4492461a96d457dd905c046aac553ba
snwakosi <snwakosi@gmail.com>
2023-05-03 07:06:54 -0700

    0*13.C

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - altered a linked list
+ * @head: pointer to the initial node in the list
+ *
+ * Return: pointer to the initial node in the new list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+
+	return (*head);
+}
